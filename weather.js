afd228e735e89ffea385a860a27b614e92d2391f
SAJDI-HABIB <sajidchughtai779@gmail.com>
2025-11-24 23:10:45 +0500

    update code of weather app

--- weather.js
+++ weather.js
@@ -1,25 +1,34 @@
 const API_KEY = "1be8080ff386d0924647880c61b6cf27";
 
-const form = document.getElementsByTagName("form");
+const form = document.querySelector("form");
 const serch = document.querySelector("#serch");
 const weather = document.querySelector("#weather");
 
 const getWeather = async (city) => {
-    const url = `
-https://api.openweathermap.org/data/3.0/weather?q=${city}&appid=${API_key}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 const response = await fetch (url);
-const data = response.json();
-//show data fuctio call here
+const data = await response.json();
+showWeather(data);
 }
 
 const showWeather = (data) => {
-    if (data.cod == "404") {
+    if (data.cod == 404 ) {
         weather.innerHTML = `<h2> city is not found <h/>`
+        return;
     }
     weather.innerHTML = `<div>
-    <img src = "" ${data.weather[0].icon}@2x.png>
+    <img src = "https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt = "weather icon">
+    </div>
+    <div>
+    <h2>${data.main.temp} °C</h2>
+    <h4>${data.weather[0].main}</h4>
     </div>`
 }
+
+form.addEventListener("submit",function (event) {
+    getWeather(serch.value);
+    event.preventDefault();
+});
 
 
 
